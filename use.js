fc24c9046129c40f91035b59c6dd70e17fac12d2
milana252 <sirinovamilana7@gmail.com>
2025-12-30 09:18:17 +0300

    Add files via upload

---
+++ use.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const { fetchData } = require('./modules/fetchData');
+const { sortStringsIgnoreSpaces } = require('./modules/sortStrings');
+const fsModule = require('./modules/fileSystem');
+
+async function run() {
+  const res = await fetchData('https://jsonplaceholder.typicode.com/users');
+  if (res.error) return console.error('Fetch error:', res.error);
+
+  const users = res.data;
+  const names = users.map(u => u.name);
+  const emails = users.map(u => u.email);
+  const sortedNames = sortStringsIgnoreSpaces(names);
+
+  const usersDir = path.join(process.cwd(), 'users');
+  await fsModule.createDir(usersDir);
+  await fsModule.writeFile(path.join(usersDir, 'names.txt'), sortedNames.join('\n'));
+  await fsModule.writeFile(path.join(usersDir, 'emails.txt'), emails.join('\n'));
+
+  console.log('Файлы успешно созданы в папке users!');
+}
+
+run();
